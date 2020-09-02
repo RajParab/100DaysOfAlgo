ff927d78d51d9804e93ff68881d595aa471b18c9
RajParab <raj.parab99@yahoo.com>
2020-09-02 22:52:15 +0530

    Day 3

--- Day 3/Subsets.py
+++ Day 3/Subsets.py
@@ -1,8 +1,17 @@
+"""
+Solution to the Leetcode Problem Subsets ->https://leetcode.com/problems/subsets/
 
-
+"""
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+	"""
+	The Solution is a recursive and uses recursion tree to solve
+
+	For every element branch out the tree with once choosing the element and once without choosing the element
+
+	"""
+    def recurssiveSubsets(self, nums):
         results=[]
         nums.append(0)
         def helper(i,element):
@@ -19,3 +28,37 @@ class Solution:
         helper(0,[])
         
         return results
+
+
+
+    def iterativeSubsets(self, nums):
+    	"""
+		Iterative logic is based on the this logi -> https://www.geeksforgeeks.org/power-set/
+
+
+		Have the range of 2**len(nums) and change the number to binary 
+		If the bit is one the number should be in the array or else not
+
+    	"""
+
+    	results=[]
+    	ele=[]
+    	
+
+    	for i in range(2**len(nums)):
+    		i_bin=bin(i)[2:].zfill(len(nums))
+    		for j in range(len(nums)):
+    			if i_bin[j]=='1':    				
+    				ele+=[nums[j]]
+    		
+    		if ele not in results:
+    			results.append(ele)
+
+    		ele=[]
+
+    	return results
+
+
+
+a=Solution()
+print(a.iterativeSubsets([1,2,3]))
